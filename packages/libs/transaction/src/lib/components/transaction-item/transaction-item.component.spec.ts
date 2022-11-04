5f6161fd80bfc4cee632368975cc4285dcb2d94f
Madipta <madipta@gmail.com>
2022-11-04 09:46:31 +0700

    rename

---
+++ packages/libs/transaction/src/lib/components/transaction-item/transaction-item.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TransactionItemComponent } from './transaction-item.component';
+
+describe('TransactionItemComponent', () => {
+  let component: TransactionItemComponent;
+  let fixture: ComponentFixture<TransactionItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TransactionItemComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TransactionItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ packages/libs/transaction/src/lib/components/transaction-item/transaction-item.component.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, IonicModule } from '@ionic/angular';
+import { ITransaction } from '@monic/libs/types';
+import { TransactionService } from '../../services/transaction-service';
+
+@Component({
+  imports: [CommonModule, IonicModule],
+  selector: 'monic-transaction-item',
+  standalone: true,
+  styleUrls: ['./transaction-item.component.scss'],
+  templateUrl: './transaction-item.component.html',
+})
+export class TransactionItemComponent {
+  @Input() transaction!: ITransaction;
+
+  constructor(
+    private alertController: AlertController,
+    private router: Router,
+    private transactionService: TransactionService
+  ) {}
+
+  edit(transaction: ITransaction) {
+    this.router.navigate(['trans-edit', transaction.id]);
+  }
+
+  async deleteAlert() {
+    const alert = await this.alertController.create({
+      header: 'Delete this transaction?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Delete',
+          role: 'confirm',
+          handler: () => {
+            this.transactionService.deleteTransaction(this.transaction.id);
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+}
