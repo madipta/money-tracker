c9efae018d8d9f9621e547b88884deb08c8f9bd7
Madipta <madipta@gmail.com>
2022-11-04 09:39:59 +0700

    make standalone

--- packages/angular-ionic/src/app/app.component.ts
+++ packages/angular-ionic/src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component, EnvironmentInjector } from '@angular/core';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 @Component({
+  imports: [IonicModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   selector: 'monic-root',
+  standalone: true,
   template: `
     <ion-app>
       <ion-content>
--- packages/angular-ionic/src/main.ts
+++ packages/angular-ionic/src/main.ts
@@ -1,13 +1,22 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { AppRoutes } from './app/app-routes';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      AngularFireModule.initializeApp(environment.firebase),
+      IonicModule.forRoot(),
+      RouterModule.forRoot(AppRoutes)
+    ),
+  ],
+}).catch((err) => console.error(err));
