014931079b521bc35f6ccbcca2592d0e18e39a42
Madipta <madipta@gmail.com>
2022-11-04 09:44:07 +0700

    rename

--- packages/angular-ionic/src/app/pages/home/trans/trans.page.ts
+++ packages/angular-ionic/src/app/pages/home/trans/trans.page.ts
@@ -2,19 +2,22 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { TransactionComponent, TransactionService } from '@monic/libs/transaction';
+import {
+  TransactionItemComponent,
+  TransactionService,
+} from '@monic/libs/transaction';
 
 @Component({
-  imports: [CommonModule, IonicModule, TransactionComponent],
+  imports: [CommonModule, IonicModule, TransactionItemComponent],
   selector: 'monic-trans',
   standalone: true,
   template: `
     <ion-content>
       <ion-list>
-        <monic-transaction
+        <monic-transaction-item
           [transaction]="exp"
           *ngFor="let exp of transactions$ | async"
-        ></monic-transaction>
+        ></monic-transaction-item>
       </ion-list>
       <ion-fab vertical="bottom" horizontal="center" slot="fixed">
         <ion-fab-button (click)="router.navigate(['trans-edit'])" side="end">
