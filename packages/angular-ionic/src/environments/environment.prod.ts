87b34641268699f88eb07cc27386bc6fc1efc04f
Madipta <madipta@gmail.com>
2022-11-24 11:38:28 +0700

    run android

--- packages/angular-ionic/src/environments/environment.prod.ts
+++ packages/angular-ionic/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyDKUsMoEZZJsRuGcbBGZaJJOxHk-NmiHjE',
+    authDomain: 'expense-tracker-6beef.firebaseapp.com',
+    projectId: 'expense-tracker-6beef',
+    storageBucket: 'expense-tracker-6beef.appspot.com',
+    messagingSenderId: '430550168397',
+    appId: '1:430550168397:web:e9d04537b3d7c1df99fd04',
+  },
 };
