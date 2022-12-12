0784156c1e29b025a57fe9c7521a065015fcccd4
Madipta <madipta@gmail.com>
2022-12-12 23:57:41 +0700

    ui redesign

--- packages/libs/angular/transaction/src/lib/components/summary/summary.component.ts
+++ packages/libs/angular/transaction/src/lib/components/summary/summary.component.ts
@@ -1,12 +1,14 @@
-import { AsyncPipe, DecimalPipe, NgIf } from '@angular/common';
+import { AsyncPipe, DecimalPipe, NgFor, NgIf } from '@angular/common';
 import {
   AfterViewInit,
+  ChangeDetectionStrategy,
   Component,
   ElementRef,
   OnDestroy,
   ViewChild,
 } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { numberShorten } from '@monic/libs/util';
 import * as echarts from 'echarts/core';
 import { LineChart } from 'echarts/charts';
 import {
@@ -19,9 +21,10 @@ import {
 } from 'echarts/components';
 import { LabelLayout, UniversalTransition } from 'echarts/features';
 import { CanvasRenderer } from 'echarts/renderers';
-import { numberShorten } from '@monic/libs/util';
-import { SummaryService } from '../../services/summary.service';
 import { Subject, takeUntil } from 'rxjs';
+import { SummaryService } from '../../services/summary.service';
+import { TransactionService } from '../../services/transaction-service';
+import { TransactionItemComponent } from '../transaction-item/transaction-item.component';
 
 echarts.use([
   CanvasRenderer,
@@ -37,74 +40,18 @@ echarts.use([
 ]);
 
 @Component({
-  imports: [AsyncPipe, DecimalPipe, IonicModule, NgIf],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    AsyncPipe,
+    DecimalPipe,
+    IonicModule,
+    NgFor,
+    NgIf,
+    TransactionItemComponent,
+  ],
   selector: 'monic-summary',
   standalone: true,
-  styles: [
-    `
-      .total-saldo {
-        align-items: center;
-        display: flex;
-        flex-direction: column;
-        margin: 16px;
-        min-height: 62px;
-
-        p {
-          color: rgba(var(--ion-color-secondary-rgb), 0.5);
-          font-size: 0.78rem;
-          margin: 0 0 4px;
-        }
-
-        h1 {
-          color: rgba(var(--ion-color-primary-rgb), 0.8);
-          font-size: 2rem;
-          font-weight: 700;
-          margin: 0;
-          padding: 0;
-        }
-      }
-
-      ion-segment {
-        background: transparent;
-      }
-
-      ion-segment-button {
-        --indicator-height: 0;
-        --padding-end: 10px;
-        --padding-start: 10px;
-        --border-radius: 16px;
-        flex: none;
-        font-size: 0.8rem;
-        letter-spacing: normal;
-        margin: 0 1px;
-        min-height: 26px;
-        min-width: 0;
-        overflow: hidden;
-
-        &.segment-button-checked {
-          background-color: rgba(var(--ion-color-primary-rgb), 0.06);
-          color: rgba(var(--ion-color-primary-rgb), 0.9);
-        }
-
-        ion-label {
-          margin: 0;
-          text-transform: none;
-        }
-      }
-
-      .chart-outer {
-        box-shadow: 1px 5px 50px rgba(var(--ion-color-primary-rgb), 0.15);
-        border-radius: 24px;
-        box-sizing: border-box;
-        padding: 16px 0 0 16px;
-        margin: 8px 16px;
-
-        div.canvas {
-          height: 70vw;
-        }
-      }
-    `,
-  ],
+  styleUrls: ['./summary.component.scss'],
   template: `
     <ion-content>
       <div class="total-saldo">
@@ -122,29 +69,47 @@ echarts.use([
           <ion-label>1 year</ion-label>
         </ion-segment-button>
       </ion-segment>
-      <div class="chart-outer">
+      <div class="sum-charts-outer">
         <div class="canvas" #sumEcharts></div>
       </div>
+      <div class="history">
+        <div class="header">Last Transactions</div>
+        <button>
+          <ion-icon name="add-circle-outline"></ion-icon>
+          <span>AddNew</span>
+        </button>
+      </div>
+      <ng-container *ngIf="transactions$ | async as transactions">
+        <monic-transaction-item
+          [transaction]="exp"
+          *ngFor="let exp of transactions"
+        ></monic-transaction-item>
+      </ng-container>
     </ion-content>
   `,
 })
 export class SummaryComponent implements AfterViewInit, OnDestroy {
   @ViewChild('sumEcharts', { static: false }) private sumEcharts!: ElementRef;
-  chart!: echarts.ECharts;
+  budgetChart!: echarts.ECharts;
+  sumChart!: echarts.ECharts;
   destroy$ = new Subject<boolean>();
   sum$ = this.summaryService.sum$;
+  transactions$ = this.transactionService.filteredTransactions$;
 
-  constructor(private summaryService: SummaryService) {}
+  constructor(
+    private summaryService: SummaryService,
+    private transactionService: TransactionService
+  ) {}
 
   ngAfterViewInit(): void {
     setTimeout(() => {
-      this.chart = echarts.init(this.sumEcharts.nativeElement);
+      this.sumChart = echarts.init(this.sumEcharts.nativeElement);
     }, 100);
-    this.summaryService.ioChartData$
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((data) => {
-        this.loadChart(data.months, data.incomes, data.outcomes);
-      });
+    // this.summaryService.ioChartData$
+    //   .pipe(takeUntil(this.destroy$))
+    //   .subscribe((data) => {
+    //     this.loadInOutcomeChart(data.months, data.incomes, data.outcomes);
+    //   });
   }
 
   ngOnDestroy(): void {
@@ -152,7 +117,7 @@ export class SummaryComponent implements AfterViewInit, OnDestroy {
     this.destroy$.unsubscribe();
   }
 
-  loadChart(months: string[], income: number[], outcome: number[]) {
+  loadInOutcomeChart(months: string[], income: number[], outcome: number[]) {
     const option = {
       legend: {
         data: ['income', 'outcome'],
@@ -184,7 +149,7 @@ export class SummaryComponent implements AfterViewInit, OnDestroy {
       ],
     };
 
-    this.chart.setOption(option);
+    this.sumChart.setOption(option);
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
