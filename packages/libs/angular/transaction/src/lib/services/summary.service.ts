1366c0463b48eb99664bc7aba4ff5f31e3d5089b
Madipta <madipta@gmail.com>
2022-12-20 21:52:19 +0700

    refactoring

--- packages/libs/angular/transaction/src/lib/services/summary.service.ts
+++ packages/libs/angular/transaction/src/lib/services/summary.service.ts
@@ -20,77 +20,69 @@ export class SummaryService {
       const thisMonth = today.getMonth();
       const startDate = new Date(thisYear, thisMonth - Number(filter), 1);
       const endDate = new Date(thisYear, thisMonth, 1);
-      return of(
-        this.firestore.collection<ITransaction>('transactions', (ref) =>
+      return this.firestore
+        .collection<ITransaction>('transactions', (ref) =>
           ref
             .where('userId', '==', user?.uid)
             .where('date', '>=', startDate)
             .where('date', '<', endDate)
             .orderBy('date', 'asc')
         )
-      );
+        .valueChanges();
     }),
-    switchMap((db) =>
-      db.valueChanges().pipe(
-        switchMap((trans) => {
-          const months: number[] = [];
-          const incomes: number[] = [];
-          const outcomes: number[] = [];
-          let income = 0;
-          let outcome = 0;
-          trans.forEach((t, index) => {
-            const month = t.date.toDate().getMonth();
-            if (!months.includes(month)) {
-              if (index > 0) {
-                incomes.push(income);
-                outcomes.push(outcome);
-              }
-              months.push(month);
-              income = 0;
-              outcome = 0;
-            }
-            const amount = Number(t.amount);
-            if (t.type === 'expense') {
-              outcome += amount;
-            } else {
-              income += amount;
-            }
-          });
-          incomes.push(income);
-          outcomes.push(outcome);
-          return of({
-            months: months.map((v) => monthlyChartNames[v]),
-            incomes,
-            outcomes,
-          });
-        })
-      )
-    )
+    switchMap((trans) => {
+      const months: number[] = [];
+      const incomes: number[] = [];
+      const outcomes: number[] = [];
+      let income = 0;
+      let outcome = 0;
+      trans.forEach((t, index) => {
+        const month = t.date.toDate().getMonth();
+        if (!months.includes(month)) {
+          if (index > 0) {
+            incomes.push(income);
+            outcomes.push(outcome);
+          }
+          months.push(month);
+          income = 0;
+          outcome = 0;
+        }
+        const amount = Number(t.amount);
+        if (t.type === 'expense') {
+          outcome += amount;
+        } else {
+          income += amount;
+        }
+      });
+      incomes.push(income);
+      outcomes.push(outcome);
+      return of({
+        months: months.map((v) => monthlyChartNames[v]),
+        incomes,
+        outcomes,
+      });
+    })
   );
   readonly sum$ = this.fireauth.user.pipe(
     switchMap((userAuth) =>
-      of(
-        this.firestore.collection<ITransaction>('transactions', (ref) =>
+      this.firestore
+        .collection<ITransaction>('transactions', (ref) =>
           ref.where('userId', '==', userAuth?.uid)
         )
-      )
+        .valueChanges()
     ),
-    switchMap((db) =>
-      db.valueChanges().pipe(
-        switchMap((trans) => {
-          let sum = 0;
-          trans.forEach((t) => {
-            const amount = Number(t.amount);
-            if (t.type === 'expense') {
-              sum -= amount;
-            } else {
-              sum += amount;
-            }
-          });
-          return of(sum);
-        })
-      )
-    )
+    switchMap((trans) => {
+      let sum = 0;
+      trans.forEach((t) => {
+        const amount = Number(t.amount);
+        if (t.type === 'expense') {
+          sum -= amount;
+        } else {
+          sum += amount;
+        }
+      });
+      return of(sum);
+    })
   );
 
   readonly last3$ = this.fireauth.user
